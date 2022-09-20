14959483abcf4f19618d27415dfb933b0d672ae3
ShakhovSergey <shahov12345@yandex.ru>
2022-09-20 20:13:29 +0300

    add-controllers fix: исправить логи в handleThrowableException

--- src/main/java/ru/practicum/shareit/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/ErrorHandler.java
@@ -16,43 +16,43 @@ public class ErrorHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException e) {
-        log.info("400 7771 {}", e.getMessage(), e);
+        log.info("400 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleValidationException(final MethodArgumentNotValidException e) {
-        log.info("400 7772 {}", e.getMessage(), e);
+        log.info("400 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleObjectAlreadyExistsException(final ObjectAlreadyExistsException e) {
-        log.info("400 7773 {}", e.getMessage(), e);
+        log.info("400 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleObjectDidntFoundException(final ObjectDidntFoundException e) {
-        log.info("404 7774 {}", e.getMessage(), e);
+        log.info("404 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleConflictException(final ConflictException e) {
-        log.info("409 7775 {}", e.getMessage(), e);
+        log.info("409 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleRuntimeException(final RuntimeException e) {
-        log.info("500 7776 {}", e.getMessage(), e);
+        log.info("500 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleThrowableException(final Throwable e) {
-        log.info("500 7777{}", e.getMessage(), e);
+        log.info("500 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
