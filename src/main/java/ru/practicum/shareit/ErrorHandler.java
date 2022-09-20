14d4e9f6b129508d320b37cc485f5e51125351a1
ShakhovSergey <shahov12345@yandex.ru>
2022-09-20 20:06:28 +0300

    add-controllers feat: добавить handleThrowableException

--- src/main/java/ru/practicum/shareit/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.exception.ConflictException;
@@ -15,31 +16,43 @@ public class ErrorHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException e) {
-        log.info("400 {}", e.getMessage(), e);
+        log.info("400 7771 {}", e.getMessage(), e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleValidationException(final MethodArgumentNotValidException e) {
+        log.info("400 7772 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleObjectAlreadyExistsException(final ObjectAlreadyExistsException e) {
-        log.info("400 {}", e.getMessage(), e);
+        log.info("400 7773 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleObjectDidntFoundException(final ObjectDidntFoundException e) {
-        log.info("404 {}", e.getMessage(), e);
+        log.info("404 7774 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleConflictException(final ConflictException e) {
-        log.info("409 {}", e.getMessage(), e);
+        log.info("409 7775 {}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleRuntimeException(final RuntimeException e) {
-        log.info("500 {}", e.getMessage(), e);
+        log.info("500 7776 {}", e.getMessage(), e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleThrowableException(final Throwable e) {
+        log.info("500 7777{}", e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
